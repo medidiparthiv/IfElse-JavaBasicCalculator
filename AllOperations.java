af352d41616aed91d9b9c2642cad7f97cddd0dd5
medidiparthiv <53120768+medidiparthiv@users.noreply.github.com>
2019-07-23 11:50:54 -0400

    Add files via upload

---
+++ AllOperations.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner y = new Scanner(System.in);
+	    Scanner x = new Scanner(System.in);
+System.out.print("Enter First Number : ");
+double a = y.nextDouble();
+System.out.print("Enter Second Number : ");
+double b = y.nextDouble();
+System.out.print("Enter Operation : ");
+String c = x.nextLine();
+if ( c.equals("+"))
+{
+System.out.println(a+b);
+}
+	    else if (c.equals("-")){
+	        System.out.println(a-b);
+	    }
+	    else if (c.equals("/")){
+	        System.out.println(a/b);
+	    }
+	    else if (c.equals("*")){
+	        System.out.println(a*b);
+	    }
+	    else{
+	        System.out.print("Not Valid Operation");
+	    }
+	}
+}
